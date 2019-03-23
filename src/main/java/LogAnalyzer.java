22296ab473d6b9a991285c5ebb6bfce4ddf62337
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-03-24 00:25:00 +0100

    Adding the possibility to change the LogAnalyzer's state (boolean wasFileNameChecked)

--- src/main/java/LogAnalyzer.java
+++ src/main/java/LogAnalyzer.java
@@ -2,14 +2,16 @@ import javax.naming.InvalidNameException;
 
 public class LogAnalyzer {
     public boolean isValidLogFileName (String fileName) throws InvalidNameException{
+        boolean wasFileNameChecked = false;
         if (fileName == null){
             throw new InvalidNameException("File Name is empty!");
         } else {
             if (fileName.endsWith(".iml") || fileName.endsWith(".IML")) {
+                wasFileNameChecked = true;
                 return true;
-            } else {
-                return false;
             }
         }
+        wasFileNameChecked = true;
+        return false;
     }
 }
--- src/test/java/LogAnalyzerValidFileExtensionTests.java
+++ src/test/java/LogAnalyzerValidFileExtensionTests.java
@@ -1,7 +1,4 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
