cb9355201387247f70b13909d093e73a70f39a5f
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-03-24 13:50:16 +0100

    Creating a AlwaysValidFakeLogAnalyzer's javaDoc

---
+++ src/main/java/AlwaysValidFakeLogAnalyzer.java
@@ -0,0 +1,13 @@
+import javax.naming.InvalidNameException;
+
+/**
+ * Class is specially named as a "(...)Fake(...)" not "(...)Mock(...)" or "(...)Stub(...)"
+ * because an object of this class may be used as a Mock (pol. Makieta) or a Stub (pol. Namiastka)
+ * According to the above, this class will have a possibility to be used multiple times in a
+ * multiple number of tests.
+ */
+public class AlwaysValidFakeLogAnalyzer implements ILogAnalyzer {
+    public boolean isValid(String fileName) throws InvalidNameException {
+        return true;
+    }
+}
